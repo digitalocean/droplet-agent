38439767f983f5f360ab197f036ecdc166dc4610
House Lee <hlee@digitalocean.com>
2021-05-20 16:05:50 -0600

    fix goimport

--- internal/sysutil/sysmanager_default.go
+++ internal/sysutil/sysmanager_default.go
@@ -4,9 +4,8 @@
 
 package sysutil
 
-
 // CopyFileAttribute copies a file's attribute to another
 // Currently this is only required for Linux environment, therefore for non-linux environment it's a no-op
-func  (s *SysManager) CopyFileAttribute(from, to string) error {
+func (s *SysManager) CopyFileAttribute(from, to string) error {
 	return nil
 }
