cd286f8ceea958e98e38423463c3781ea67f68d9
House Lee <hlee@digitalocean.com>
2022-02-02 18:20:18 -0800

    update mocks

--- internal/metadata/actioner/internal/mocks/ssh_mgr_mocks.go
+++ internal/metadata/actioner/internal/mocks/ssh_mgr_mocks.go
@@ -34,6 +34,20 @@ func (m *MocksshManager) EXPECT() *MocksshManagerMockRecorder {
 	return m.recorder
 }
 
+// RemoveDoTTYKeys mocks base method.
+func (m *MocksshManager) RemoveDoTTYKeys() error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RemoveDoTTYKeys")
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// RemoveDoTTYKeys indicates an expected call of RemoveDoTTYKeys.
+func (mr *MocksshManagerMockRecorder) RemoveDoTTYKeys() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RemoveDoTTYKeys", reflect.TypeOf((*MocksshManager)(nil).RemoveDoTTYKeys))
+}
+
 // UpdateKeys mocks base method.
 func (m *MocksshManager) UpdateKeys(keys []*sysaccess.SSHKey) error {
 	m.ctrl.T.Helper()
